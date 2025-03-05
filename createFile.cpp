3825290f0025fc46917a98926093f2384d71c316
SorrowGod <1431844785@qq.com>
2025-03-05 14:26:09 +0800

    创建file
    the first time to use GitHub

---
+++ createFile.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+using namespace std;
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        int n = s.length(), ans = 0, left = 0;
+        unordered_map<char, int> cnt;
+        for (int right = 0; right < n; right++)
+        {
+            char c = s[right];
+            cnt[c]++;
+            while (cnt[c] > 1)
+            {
+                cnt[s[left]]--;
+                left++;
+            }
+            ans = max(ans, right - left + 1);
+        }
+        return ans;
+    }
+};
+int main()
+{
+    cout << "hello";
+    system("pause");
+    return 0;
+}
